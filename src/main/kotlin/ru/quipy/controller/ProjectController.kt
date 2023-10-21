06e20c4d0c6273d5f966b4466af2a82024963505
nkpopkov <nkpopkov@mts.ru>
2023-10-22 00:23:14 +0300

    ktlint: added plugin and formatting

--- src/main/kotlin/ru/quipy/controller/ProjectController.kt
+++ src/main/kotlin/ru/quipy/controller/ProjectController.kt
@@ -18,21 +18,21 @@ import java.util.*
 @RestController
 @RequestMapping("/projects")
 class ProjectController(
-    val projectEsService: EventSourcingService<UUID, ProjectAggregate, ProjectAggregateState>
+    val projectEsService: EventSourcingService<UUID, ProjectAggregate, ProjectAggregateState>,
 ) {
 
     @PostMapping("/{projectTitle}")
-    fun createProject(@PathVariable projectTitle: String, @RequestParam creatorId: String) : ProjectCreatedEvent {
+    fun createProject(@PathVariable projectTitle: String, @RequestParam creatorId: String): ProjectCreatedEvent {
         return projectEsService.create { it.create(UUID.randomUUID(), projectTitle, creatorId) }
     }
 
     @GetMapping("/{projectId}")
-    fun getAccount(@PathVariable projectId: UUID) : ProjectAggregateState? {
+    fun getAccount(@PathVariable projectId: UUID): ProjectAggregateState? {
         return projectEsService.getState(projectId)
     }
 
     @PostMapping("/{projectId}/tasks/{taskName}")
-    fun createTask(@PathVariable projectId: UUID, @PathVariable taskName: String) : TaskCreatedEvent {
+    fun createTask(@PathVariable projectId: UUID, @PathVariable taskName: String): TaskCreatedEvent {
         return projectEsService.update(projectId) {
             it.addTask(taskName)
         }
